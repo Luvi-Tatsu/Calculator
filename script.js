0f5f16d95be207d054bb3f7fb505669173f52962
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-04-15 19:55:08 +0800

    create the css outline

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 
 
-function add(){
-    const firstNum = prompt();
-    const secontNum = prompt();
-    ans = parseInt(firstNum) + parseInt(secontNum);
-    return console.log(ans); 
-}
-add()
+// function add(){
+//     const firstNum = prompt();
+//     const secontNum = prompt();
+//     ans = parseInt(firstNum) + parseInt(secontNum);
+//     return console.log(ans); 
+// }
+// add()
