abef669c4da31589fc05911a409748f894110b3d
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-04-15 11:19:36 +0800

    simple addition using prompt

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // console.log("Hello World");
 
+
+
+function add(){
+    const firstNum = prompt();
+    const secontNum = prompt();
+    ans = parseInt(firstNum) + parseInt(secontNum);
+    return console.log(ans); 
+}
+add()
