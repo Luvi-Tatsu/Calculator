c64cb99f78b5440711683c1397c08767e38eb546
Luvi-Tatsu <195727939+Luvi-Tatsu@user.noreply.github.com>
2025-05-05 13:40:00 +0800

    fix some bugs

--- readme.txt
+++ readme.txt
@@ -2,7 +2,8 @@ Calculator Project starts at april 15 2025
 
 
 finished May 5 2025 idea of the project but cannot fix all the bugs.
-cannot implement extra credit either. 
-will try to finish in the future. however I want to proceed to the next lessons.
-dot still has a bug
-error when  you add more in total
+
+dot still has a bug //fixed
+error when  you add more in total //fixed
+cannot implement all extra credit either. 
+no keyboard support and backspace button 
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let firstNumber = "";
-let secondNumber = "" ;
+let secondNumber = "";
 let operator = 0;
 let dote = 0;
 let total = 0;
@@ -30,42 +30,59 @@ changeOp.addEventListener("click",function (){
 
 const seven = document.querySelector(".seven");
 seven.addEventListener("click",function (){
-     inputArea.textContent += "7";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "7";}
+     else{inputArea.textContent += "7";}
      if(operator === 0){
      firstNumber += 7;}
      else {secondNumber += 7;}})
 
 const eight = document.querySelector(".eight");
-eight.addEventListener("click",function (){
-     inputArea.textContent += "8";
-     if(operator === 0){
-     firstNumber += 8;}
-     else {secondNumber += 8;}})
-
+eight.addEventListener("click",function (){ 
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "8";}
+     else{inputArea.textContent += "8";}
+     if(operator === 0){firstNumber += 8;}
+     else {secondNumber += 8; }})
+     
 const nine = document.querySelector(".nine");
 nine.addEventListener("click",function (){
-     inputArea.textContent += "9";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "9";}
+     else{inputArea.textContent += "9";}
      if(operator === 0){
      firstNumber += 9;}
      else {secondNumber += 9;}})
 
 const four = document.querySelector(".four");
 four.addEventListener("click",function (){
-     inputArea.textContent += "4";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "4";}
+     else{inputArea.textContent += "4";}
      if(operator === 0){
      firstNumber += 4;}
      else {secondNumber += 4;}})
 
 const five = document.querySelector(".five");
 five.addEventListener("click",function (){
-     inputArea.textContent += "5";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "5";}
+     else{inputArea.textContent += "5";}
      if(operator === 0 ){
      firstNumber += 5;}
      else {secondNumber += 5; }})
 
 const six = document.querySelector(".six");
 six.addEventListener("click",function (){
-     inputArea.textContent += "6";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "6";}
+     else{inputArea.textContent += "6";}
      if(operator === 0){
      firstNumber += 6;}
      else {secondNumber += 6;}
@@ -73,37 +90,51 @@ six.addEventListener("click",function (){
 
 const one = document.querySelector(".one");
 one.addEventListener("click",function (){
-     inputArea.textContent += "1";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "1";}
+     else{inputArea.textContent += "1";}
      if(operator === 0){
      firstNumber +=1;}
      else {secondNumber += 1;}
 })
 const two = document.querySelector(".two");
 two.addEventListener("click",function (){
-     inputArea.textContent += "2";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "2";}
+     else{inputArea.textContent += "2";}
      if(operator === 0){
      firstNumber += 2;}
      else {secondNumber += 2;}
 })
 const three = document.querySelector(".three");
 three.addEventListener("click",function (){
-     inputArea.textContent += "3";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "3";}
+     else{inputArea.textContent += "3";}
      if(operator === 0){
      firstNumber += 3;}
      else {secondNumber += 3;}
 })
 const zero = document.querySelector(".zero");
 zero.addEventListener("click",function (){
-     inputArea.textContent += "0";
+     let txt = inputArea.textContent;
+     if(txt === " + " || txt === " - " || txt === " * " || txt === " / " ||txt === " % " )
+          {inputArea.textContent = "0";}
+     else{inputArea.textContent += "0";}
      if(operator === 0){
      firstNumber += 0;}
      else {secondNumber += 0;}})
 
 const dot = document.querySelector(".dot");
-dot.addEventListener("click",function (){ 
-     if(!firstNumber.includes(".") && operator === 0){
-     firstNumber += "."; inputArea.textContent += ".";}
-     if(!secondNumber.includes(".") && operator != 0){secondNumber += "."; inputArea.textContent += "."}})
+     dot.addEventListener("click",function (){ 
+          if(!inputArea.textContent.includes(".")){
+          inputArea.textContent += "."; ; }
+          if(operator === 0){
+               firstNumber += "."}
+               else {secondNumber += ".";}})
 
 const equals = document.querySelector(".equals");
 equals.addEventListener("click",function (){
@@ -111,50 +142,50 @@ equals.addEventListener("click",function (){
 
 const percentile = document.querySelector(".percentile");
 percentile.addEventListener("click",function (){
-     inputArea.textContent += " % ";
+     inputArea.textContent = " % ";
      if(operator === 0){operator = 1;}
      if(operator != 0 && secondNumber != ""){
           operand(firstNumber, secondNumber);
           operator = 1;
-          inputArea.textContent += " % ";}})
+          inputArea.textContent = " % ";}})
 
 const dev = document.querySelector(".dev");
 dev.addEventListener("click",function (){
-     inputArea.textContent += " / ";
+     inputArea.textContent = " / ";
      if(operator === 0){operator = 2;}
      if(operator != 0 && secondNumber != ""){
           operand(firstNumber, secondNumber);
           operator = 2;
-          inputArea.textContent += " / ";}})
+          inputArea.textContent = " / ";}})
 
 const mul = document.querySelector(".mul");
 mul.addEventListener("click",function (){
-     inputArea.textContent += " * "; 
+     inputArea.textContent = " * "; 
      if(operator === 0){operator = 3;}
      if(operator != 0 && secondNumber != ""){
           operand(firstNumber, secondNumber);
           operator = 3;
-          inputArea.textContent += " * ";}})
+          inputArea.textContent = " * ";}})
 
 const sub = document.querySelector(".sub");
 sub.addEventListener("click",function (){
-     inputArea.textContent += " + ";
+     inputArea.textContent = " + ";
      if(operator === 0){operator = 4;}
      if(operator != 0 && secondNumber != ""){
           operand(firstNumber, secondNumber);
           operator = 4;
-          inputArea.textContent += " - ";}})
+          inputArea.textContent = " - ";}})
 
      
 
 const add = document.querySelector(".add");
 add.addEventListener("click",function (){
-     inputArea.textContent += " + ";
+     inputArea.textContent = " + ";
      if(operator === 0){operator = 5;}
      if(operator != 0 && secondNumber != ""){
           operand(firstNumber, secondNumber);
           operator = 5;
-          inputArea.textContent += " + ";}})
+          inputArea.textContent = " + ";}})
           
 
 
@@ -164,10 +195,12 @@ function operand(){
            firstNumber = parseFloat(firstNumber);
            secondNumber = parseFloat(secondNumber);
           total = firstNumber + secondNumber;
-          firstNumber = total;
+          total = Math.round(total*100)/100;
+          firstNumber = `${total}`;
           secondNumber = "";
           inputArea.textContent = `${total}`
-          console.log(firstNumber);}
+          console.log(firstNumber); console.log(secondNumber);}
+          
 
      
           else if(operator === 4){
@@ -175,6 +208,7 @@ function operand(){
           firstNumber = parseFloat(firstNumber);
            secondNumber = parseFloat(secondNumber);
           let total = firstNumber - secondNumber;
+          total = Math.round(total*100)/100;
           firstNumber = total;
           secondNumber = "";
           inputArea.textContent = `${total}`}
@@ -184,6 +218,7 @@ function operand(){
           firstNumber = parseFloat(firstNumber);
            secondNumber = parseFloat(secondNumber);
           let total = firstNumber * secondNumber;
+          total = Math.round(total*100)/100;
           firstNumber = total;
           secondNumber = "";
           inputArea.textContent = `${total}`}
@@ -193,6 +228,7 @@ function operand(){
           firstNumber = parseFloat(firstNumber);
           secondNumber = parseFloat(secondNumber);
           let total = firstNumber / secondNumber;
+          total = Math.round(total*100)/100;
           firstNumber = total;
           secondNumber = "";
           inputArea.textContent = `${total}`}
@@ -202,6 +238,7 @@ function operand(){
           firstNumber = parseFloat(firstNumber);
            secondNumber = parseFloat(secondNumber);
           let total = firstNumber % secondNumber;
+          total = Math.round(total*100)/100;
           firstNumber = total;
           secondNumber = "";
           inputArea.textContent = `${total}`}
